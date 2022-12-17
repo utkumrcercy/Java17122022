8bb388cdf00cb6c4e5de2f4fd889dee1a43a76e2
Mehmet Nuri ÖZTÜRK <mnozturk@outlook.com.tr>
2022-12-17 18:51:57 +0300

    Add files via upload

---
+++ Merhaba/src/Main.java
@@ -0,0 +1,86 @@
+public class Main{
+
+    /**
+     * Bu modül şu işe yarar
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        // Yorum satırı
+
+        /**
+         * Yorum satırı
+         */
+
+        /* kljasdkjaskld*/
+
+
+        // bu  ekrana merhaba java yazar
+
+        // Değişken tanımlama
+
+
+        // Değer atayarak değişken tanımlama
+        String merhaba = "Merhaba";
+        String merhaba2 ="Merhaba 2";
+        String merhaba_dunya = "Merhaba Dünya";
+
+        // Değer atamadan değişken tanımlama
+        String Merhaba;
+
+        Merhaba = "Merhaba 2";
+
+        System.out.println(merhaba);
+
+        System.out.println(Merhaba);
+
+        /**
+         * Yazım Standartları
+         *
+         * PascalCase
+         * FULLCASE
+         * camelCase
+         * snake_case
+         * kebab-case
+         *
+         */
+
+        String buGununTarihi = "17.12.2022";
+
+        long uzunDeger = 65l;
+        System.out.println(uzunDeger);
+
+        char aKarakteri = 'A';
+        System.out.println(aKarakteri);
+
+        float floatDegerim = 16.65f;
+        System.out.println(floatDegerim);
+
+        double doubleDegerim =  20.00036d;
+        System.out.println(doubleDegerim);
+
+        boolean varMi = false;
+
+        System.out.println(varMi);
+
+        System.out.println("----------------------------------");
+
+        byte byteDegiskeni = 34;
+        short shortDegiskeni = 4567;
+        int integerDegeri = 10000000;
+        long longDegeri = 54654654654654654l;
+        char charDegiskeni = 'A';
+        float floatDegiskeni = 54.54654f;
+        double doubleDegiskeni = 45.65465465465d;
+        boolean booleanDegiskeni = false;
+
+        System.out.println("Byte Değişkenimin Değeri = " + byteDegiskeni);
+        System.out.println("Short Değişkenimin Değeri = " + shortDegiskeni);
+        System.out.println("Integer Değişkenimin Değeri = " + integerDegeri);
+        System.out.println("Long Değişkenimin Değeri = " + longDegeri);
+        System.out.println("Char Değişkenimin Değeri = " + charDegiskeni);
+        System.out.println("Float Değişkenimin Değeri = " + floatDegiskeni);
+        System.out.println("Double Değişkenimin Değeri = " + doubleDegiskeni);
+        System.out.println("Boolean Değişkenimin Değeri = " + booleanDegiskeni);
+    }
+}
